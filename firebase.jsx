f32a766e1922bfa550ef0cc3aaa89a6ba1cb1e4b
rojamuthiah <143870492+rojamuthiah@users.noreply.github.com>
2024-10-18 16:45:34 +0530

    Create firebase.jsx

---
+++ firebase.jsx
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIyLGjcTHckSYDeb4BUvlgL9oZVDji2w4",
+    authDomain: "movie-382e8.firebaseapp.com",
+    projectId: "movie-382e8",
+    storageBucket: "movie-382e8.appspot.com",
+    messagingSenderId: "481895442119",
+    appId: "1:481895442119:web:daa5418539a93fb24780e2",
+    measurementId: "G-H1Q2ZD30D6"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
